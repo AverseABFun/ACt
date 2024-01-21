f1abc1c49f3ad4c6a44f91e2ecf2c981768a6f98
Arthur <averse.abfun@gmail.com>
2024-01-21 09:57:45 -0600

    misc stuff

--- frontend/App.js
+++ frontend/App.js
@@ -2,11 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MapLibreGL from '@maplibre/maplibre-react-native';
+import { Header } from 'react-native/Libraries/NewAppScreen';
+
+MapLibreGL.setAccessToken(null); // this is apparently necessary due to an android bug
+
+const apiKey = process.env.MAPLIBRE_TOKEN; // and you thought i would give away my api token, huh?
+const styleUrl = `https://tiles.stadiamaps.com/styles/alidade_smooth.json?api_key=${apiKey}`;
 
 function MapPage() {
   return (
     <View style={styles.container}>
-      <Text>Map</Text>
+      <Header 
+        leftComponent={null}
+        centerComponent={{ text: 'Map', style: { color: '#fff' } }}
+        rightComponent={null}
+        containerStyle={styles.header}
+      />
+      <MapLibreGL.MapView
+          style={styles.map}
+          styleURL={styleUrl}
+        />
       <StatusBar style="auto" />
     </View>
   );
@@ -30,4 +46,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+  header: {
+    backgroundColor: '#222',
+    justifyContent: 'space-around',
+  }
 });
